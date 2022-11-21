33df3301251ae305daea269169d29ad5b59b1fe3
imsori <dlathfl868@naver.com>
2022-11-21 18:55:43 +0900

    servlet

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,69 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ResponseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //HttpServletResponse 역할
+        //HTTP 응답 메시지 생성 HTTP 응답코드 지정 헤더 생성
+        //바디 생성
+
+        //[status-line]
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        //[response-headers]
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setHeader("Cache-Control","no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma","no-cache");
+        response.setHeader("my-header","hello");
+
+        //[Header 편의 메소드] -편의 기능 제공
+        //Content-Type, 쿠키, Redirect
+
+        //content(response);
+        //cookie(response);
+        redirect(response);
+
+
+        PrintWriter writer = response.getWriter();
+        writer.println("ok"); //http 응답 데이터 - 단순 텍스트
+
+    }
+
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8")
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //response.setContentLength(2); //(생략시 자동 생성)
+    }
+
+    private void cookie(HttpServletResponse response) {
+        //Set-Cookie: myCookie=good; Max-Age=600;
+        //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+
+        //response.setStatus(HttpServletResponse.SC_FOUND); //302
+        //response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
+    }
+}
---
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,37 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+    
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-type: application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        //HTTP 응답으로 JSON을 반환할 때는 content-type을 application/json 로 지정해야 한다.
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        //{"username":"kim", "age":20}
+        //Jackson 라이브러리가 제공하는 objectMapper.writeValueAsString() 를 사용하면 객체를 JSON 문자로 변경할 수 있다.
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+
+
+
+    }
+}
